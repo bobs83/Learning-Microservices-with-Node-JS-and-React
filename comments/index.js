edf5d014a1999cc84e8bcb6c485a59f72faf5854
Bobby Singh <b830103@gmail.com>
2024-09-14 22:27:49 +0200

    created and tested the comment service

---
+++ comments/index.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const crypto = require("crypto");
+
+const app = express();
+app.use(bodyParser.json());
+
+const commentsByPostId = {};
+
+app.get("/posts/:id/comments", (req, res) => {
+  res.send(commentsByPostId[req.params.id] || []);
+});
+
+app.post("/posts/:id/comments", (req, res) => {
+  const commentsId = crypto.randomBytes(4).toString("hex");
+  const { content } = req.body;
+  const comments = commentsByPostId[req.params.id] || [];
+
+  comments.push({ id: commentsId, content });
+
+  commentsByPostId[req.params.id] = comments;
+
+  res.status(201).send(comments);
+});
+
+app.listen(3001, () => {
+  console.log("Listening on port 3001");
+});
