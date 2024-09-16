3520e6cb286ef6adf518f410424aaef32f14b65e
Bobby Singh <b830103@gmail.com>
2024-09-16 02:32:05 +0200

    Created the basic React app
    Created the basic components in React, which we will build our microservices app upon.

--- comments/index.js
+++ comments/index.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const crypto = require("crypto");
+const cors = require("cors");
 
 const app = express();
 app.use(bodyParser.json());
+app.use(cors());
 
 const commentsByPostId = {};
 
